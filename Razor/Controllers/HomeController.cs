07d179ccbf76443e0ce93dc034273886a38ca654
jennyf19 <ferriesj@gmail.com>
2016-11-21 10:11:14 -0800

    added conditionals and if statements to razor view

--- Razor/Controllers/HomeController.cs
+++ Razor/Controllers/HomeController.cs
@@ -3,28 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Razor.Models;
 
 namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
+        Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
         public ActionResult Index()
         {
-            return View();
+            return View(myProduct);
         }
 
-        public ActionResult About()
+        public ActionResult NameAndPrice()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return View(myProduct);
         }
 
-        public ActionResult Contact()
+        //Pass data to the view using a model object and the view bag
+        public ActionResult DemoExpression()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
 
-            return View();
+            return View(myProduct);
         }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 28.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 12.23M},
+            };
+            return View(array);
+        }
+/*public ActionResult Index()
+                {
+                    return View();
+                }
+        
+                public ActionResult About()
+                {
+                    ViewBag.Message = "Your application description page.";
+        
+                    return View();
+                }
+        
+                public ActionResult Contact()
+                {
+                    ViewBag.Message = "Your contact page.";
+        
+                    return View();
+                }*/
     }
 }
